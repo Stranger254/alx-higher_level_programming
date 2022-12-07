8e8e60e1dc47750bdeac9df741152ca633f6616c
Stranger254 <muhiatomkys@gmail.com>
2022-12-07 19:40:13 +0300

    Matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def square_matrix_simple(matrix=[]):
+    dup_matrix = matrix.copy()
+    for line in range(len(matrix)):
+        dup_matrix[line] = list(map((lambda x: x**2), matrix[line]))
+    return(dup_matrix)
