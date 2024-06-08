81f89c943a93817c5dadee44a56be9f30424a53e
Tomkys Muhia <99245417+Stranger254@users.noreply.github.com>
2024-06-08 18:30:41 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-function nbOccurences (list, searchElement) {
-    const nb = list.reduce((ac, e) => e === searchElement ? ac + 1 : ac, 0);
-    return nb;
-  }
-  
-  exports.nbOccurences = nbOccurences;
-  
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-exports.esrever = function (list) {
-    const invertedList = [];
-    for (const e of list) invertedList.unshift(e);
-    return invertedList;
-  };
-  
